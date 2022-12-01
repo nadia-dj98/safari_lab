88b9d3c2844ba06b7221627029cf05d0924b829b
Nadia <nadia_dj@hotmail.co.uk>
2022-12-01 15:45:15 +0000

    completed mvp

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,1 +1,7 @@
-SELECT age FROM animals;
+--MVP1
+SELECT animals.name, enclosure.name AS enclosure_name 
+FROM animals INNER JOIN enclosure ON animals.enclosure_id = enclosure.id;
+
+SELECT enclosure.name AS enclosure_name, staff.name AS staff_name
+FROM enclosure INNER JOIN assignments ON enclosure.id = assignments.enclosure_id
+INNER JOIN staff ON assignments.employee_id = staff.id;
